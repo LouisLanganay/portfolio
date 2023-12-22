aced3c3bde1a60fb274e86debc8deb44235d8b13
LouisLanganay <louis.langanay@epitech.eu>
2023-12-22 18:36:55 +0100

    fix: Update import statement in Layout component
    Fixed import statement in Layout component to import SpeedInsights from '@vercel/speed-insights/react' instead of '@vercel/speed-insights/next'. This resolves an issue with the import path.

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { Header, Footer } from '../components';
 import PropTypes from 'prop-types';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 interface LayoutProps {
   children: React.ReactNode;
